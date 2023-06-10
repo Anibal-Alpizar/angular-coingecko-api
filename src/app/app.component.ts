921edfb18376b45d65661cfc23bea5d94ec47326
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-06-10 12:32:12 -0600

    (fix): call api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.http
       .get(
-        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en'
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
       )
       .subscribe(
         (res) => {

92224e5970d05b6335479a9c4b6123424e1290a0
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-06-10 12:51:04 -0600

    jsonplaceholder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,7 +24,7 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.http
       .get<Coin[]>(
-        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+        'https://jsonplaceholder.typicode.com/users'
       )
       .subscribe(
         (res) => {
